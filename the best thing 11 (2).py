129f49c4c0e1273912833d5106b1ba1ab5545b03
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-03 15:26:20 -0700

    Update the best thing 11 (2).py

--- the best thing 11 (2).py
+++ the best thing 11 (2).py
@@ -33,7 +33,8 @@ if 'feed' not in st.session_state:
  st.session_state.feed=0
 if 'hearts' not in st.session_state:
  st.session_state.hearts=3
-
+if 'coins' not in st.session_state:
+ st.session_state.coins=0
 
 num1 = st.session_state.num1
 num2 = st.session_state.num2
